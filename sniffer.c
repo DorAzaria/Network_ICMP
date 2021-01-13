c06bc812bb5c340b7a352225ff893c48604d385a
dor <dorazaria@gmail.com>
2021-01-13 21:54:50 +0200

    final

--- sniffer.c
+++ sniffer.c
@@ -41,106 +41,107 @@
  * -> Allows programmers to specify filtering rules using
  *    human readable Boolean expressions.
  **********************************************/
-
-#include<netinet/in.h>
-#include<stdio.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
-#include<string.h>
-#include<netinet/ip_icmp.h>
-#include<netinet/ip.h>
-#include<net/ethernet.h>
-#include <sys/types.h>
 #include <sys/ioctl.h>
-#include<sys/socket.h>
-#include<arpa/inet.h>
-#include <linux/if_packet.h>
-#include <linux/if_ether.h>
-#include <linux/in.h>
 #include <net/if.h>
-#include <unistd.h>
-#include <pcap.h>
-#define ICMP_HDR_LEN 4
+#include <netinet/ether.h>
+#include <netinet/ip_icmp.h>
+#include <netinet/ip.h>
+#include <sys/socket.h>
+#include <linux/if_packet.h>
+#include <net/ethernet.h>
 
 /**********************************************
  * Implementation:
  **********************************************/
+int count=0;
+void got_packet(unsigned char* , int );
 
-void print_icmp_packet(char* , int);
-int icmp=0;
+int main(int argc, char *argv[]) {
 
-int main() {
+    printf("\n######################################################\n");
+    printf("   Welcome!, please wait for new ICMP packets...\n");
+    printf("######################################################\n\n");
+
     int PACKET_LEN = IP_MAXPACKET;
-    struct sockaddr saddr;
-    struct ifreq ethreq;
     struct packet_mreq mr;
 
 /**********************************************
  * Create the raw socket
  * -> htons(ETH_P_ALL): Capture all types of packets
  **********************************************/
-    int sock;
-    if ( (sock=socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL))) < 0) {
-        perror("\nRaw socket create failed.\n");
-        exit(1);
+    int raw_socket;
+    if ((raw_socket = socket(PF_PACKET, SOCK_RAW, htons(ETH_P_ALL))) == -1) {
+        perror("listener: socket");
+        return -1;
     }
 
 /**********************************************
- * THIS PART ISN'T WORKING!
- * // set NIC work on promiscuous mode
-   // Turn on the promiscuous mode.
-    mr.mr_type = PACKET_MR_PROMISC;
-    setsockopt(sock, SOL_PACKET, PACKET_ADD_MEMBERSHIP, &mr, sizeof(mr));
-    strncpy(ethreq.ifr_name,"eth3", IFNAMSIZ);
-    if (ioctl(sock, SIOCGIFFLAGS, &ethreq)==-1) {
-        perror("Promiscuous mode set failed.\n");
-        close(sock);
-        exit(1);
-    }
+ * Turn on the promiscuous mode
  **********************************************/
+    mr.mr_type = PACKET_MR_PROMISC;
+    setsockopt(raw_socket, SOL_PACKET, PACKET_ADD_MEMBERSHIP, &mr, sizeof(mr));
 
 /**********************************************
  * Get captured packet
  **********************************************/
-    char buffer[IP_MAXPACKET];
-    socklen_t len;
-    while (1) {
-        bzero(buffer,IP_MAXPACKET);
-        len = sizeof(saddr);
-        int data_size = recvfrom(sock, buffer, PACKET_LEN, 0, &saddr, &len);
+    char buffer[PACKET_LEN];
+    while(1) {
+        bzero(buffer,PACKET_LEN);
+        int received = recvfrom(raw_socket, buffer, ETH_FRAME_LEN, 0, NULL, NULL);
 
-        if (data_size >= 0){
+        unsigned char* hex= ((unsigned char*)buffer);
+        unsigned char* packet= (unsigned char *)malloc(received);
 
-            struct iphdr *iph = (struct iphdr *) (buffer + sizeof(struct ethhdr));
-            struct sockaddr_in source, dest;
+        for (int i =14,j=0; i < received-4;i++,j++) {
+            packet[j]=hex[i];
+        }
 
- /**********************************************
- * Check if it's a ICMP protocol.
- **********************************************/
-            if (iph->protocol == IPPROTO_ICMP) {
+        got_packet(packet, received);
+    }
+}
 
-                printf("============================================");
-                printf("\nICMP packet %d, data size: %d \n", ++icmp, data_size);
+void got_packet(unsigned char* buffer, int size) {
 
-                memset(&source, 0, sizeof(source));
-                source.sin_addr.s_addr = iph->saddr;
+    struct iphdr *iph = (struct iphdr*)buffer;
 
-                memset(&dest, 0, sizeof(dest));
-                dest.sin_addr.s_addr = iph->daddr;
+/**********************************************
+* Check if it's a ICMP protocol.
+**********************************************/
+    if (iph->protocol == 1) {
 
-                printf("|-IP Header\n");
-                printf("   |-Source IP        : %s\n",inet_ntoa(source.sin_addr));
-                printf("   |-Destination IP   : %s\n",inet_ntoa(dest.sin_addr));
+        unsigned short iphdrlen = iph->ihl*4;
+        struct icmphdr *icmph = (struct icmphdr *)(buffer + iphdrlen);
 
-                int iphdrlen = iph->ihl * ICMP_HDR_LEN;
-                struct icmphdr *icmph = (struct icmphdr *) (buffer + iphdrlen + sizeof(struct ethhdr));
-                printf("|-ICMP Header\n");
-                printf("   |-Type             : %d\n", (unsigned int)(icmph->type));
-                printf("   |-Code             : %d\n", icmph->code);
+/**********************************************
+* If the IP Destination is known,
+* then print the data.
+**********************************************/
+        if((unsigned int)(icmph->type) != 96) {
 
-            }
+            struct sockaddr_in source,dest;
+            memset(&source, 0, sizeof(source));
+            source.sin_addr.s_addr = iph->saddr;
+
+            memset(&dest, 0, sizeof(dest));
+            dest.sin_addr.s_addr = iph->daddr;
+
+            printf("*********************** ICMP Packet No. %d *************************\n",++count);
+            printf("\nIP Header\n");
+            printf("---> Source IP        : %s\n",inet_ntoa(source.sin_addr));
+            printf("---> Destination IP   : %s\n",inet_ntoa(dest.sin_addr));
+            printf("\nICMP Header\n");
+            printf("---> Type : %d\n", (unsigned int) (icmph->type));
+            printf("---> Code : %d\n", (unsigned int) (icmph->code));
+
         }
     }
-
-    close(sock);
-    return 0;
 }
+
+
+
+
+
